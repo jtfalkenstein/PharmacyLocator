88b65c801fa5d330afcc1abe1ea583eb2e4ec5b0
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-10-17 16:02:11 -0500

    moving global exception handler to application

--- PharmacyLocator/Bootstrapper/Bootstrapper.php
+++ PharmacyLocator/Bootstrapper/Bootstrapper.php
@@ -16,19 +16,15 @@ use const ROOT;
 class Bootstrapper {
     public static function boot(){
         //Global exception handler
-        try{
-            $container = $this->getDIContainer();
-            $app = $container->get(Application::class);
-            $app->run();
-        } catch (Exception $ex) {
-            //Do something about it here...
-        }
+        $container = SELF::getDIContainer();
+        $app = $container->get(Application::class);
+        $app->run();
     }
     
     
     private static function getDIContainer(): ContainerInterface{
         $builder = new ContainerBuilder();
-        $builder->addDefinitions(include ROOT . DS . 'config' . DS . 'di-definitions.php');
+        $builder->addDefinitions(include implode(DS, [ROOT, 'config', 'di-definitions.php']));
         return $builder->build();
     }
 }
