a6682460d7c7124841e7474159daaced5a306880
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-10-17 15:17:27 -0500

    created pharmacyInfo class to capture info about nearest Pharmacy

---
+++ PharmacyLocator/Repository/PharmacyInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PharmacyLocator\Repository;
+
+/**
+ * Description of PharmacyInfo
+ *
+ * @author jfalkenstein
+ */
+class PharmacyInfo {
+    public $id;
+    public $name;
+    public $address;
+    public $city;
+    public $state;
+    public $zip;
+    public $distance;
+}
