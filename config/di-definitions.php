a669376cfe096588f82654b2d923574a696008ba
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-10-17 15:58:15 -0500

    fixing namespace on response classes

--- config/di-definitions.php
+++ config/di-definitions.php
@@ -4,8 +4,11 @@ use PharmacyLocator\ConfigManager\ConfigManager;
 use PharmacyLocator\ConfigManager\IConfigManager;
 use PharmacyLocator\Repository\IPharmacyRepo;
 use PharmacyLocator\Repository\SqlitePharmacyRepo;
+use PharmacyLocator\Responses\IResponseSerializer;
+use PharmacyLocator\Responses\JsonResponseSerializer;
 use function DI\object;
 return [
     IPharmacyRepo::class => object(SqlitePharmacyRepo::class),
-    IConfigManager::class => object(ConfigManager::class)
+    IConfigManager::class => object(ConfigManager::class),
+    IResponseSerializer::class => object(JsonResponseSerializer::class)
 ];
